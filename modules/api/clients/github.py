cea6f4291bde5690588be74d28d6538555180944
KornTTT <kornttt88@gmail.com>
2024-01-16 14:31:31 +0200

    Added github api client

---
+++ modules/api/clients/github.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class GithHub:
+    def get_user(self, username):
+        r = requests.get(f"https://api.github.com/users/{username}")
+        body = r.json()
+
+        return body
+
+    def search_repo(self, name):
+        r = requests.get(
+            "https://api.github.com/search/repositories", params={"q": name}
+        )
+        body = r.json()
+        return body
+
+    def get_emoji(self):
+        r = requests.get("https://api.github.com/emojis")
+        body = r.json()
+        return body
+
+    def get_commit(self, owner, repo):
+        r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
+        body = r.json()
+        return body
