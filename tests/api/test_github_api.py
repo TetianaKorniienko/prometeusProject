4857c248ff09d40a2c77352f954e05207e61f01d
TanyaKorniienko <98847662+TanyaKorniienko@users.noreply.github.com>
2024-01-16 14:34:42 +0200

    Update test_github_api.py

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -2,16 +2,16 @@ import pytest
 from modules.api.clients.github import GithHub
 
 
-# @pytest.mark.api
-# def test_user_exists(github_api):
-#     user = github_api.get_user("defunkt")
-#     assert user["login"] == "defunkt"
+@pytest.mark.api
+def test_user_exists(github_api):
+    user = github_api.get_user("defunkt")
+    assert user["login"] == "defunkt"
 
 
-# @pytest.mark.api
-# def test_user_not_exists(github_api):
-#     r = github_api.get_user("butenkosergii")
-#     assert r["message"] == "Not Found"
+@pytest.mark.api
+def test_user_not_exists(github_api):
+    r = github_api.get_user("butenkosergii")
+    assert r["message"] == "Not Found"
 
 
 @pytest.mark.api
